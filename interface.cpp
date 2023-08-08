8c9cd69b121dd84575831dffb5e87abaaddfe9ef
mateistefann <matei.tarziu@gmail.com>
2023-08-08 17:26:23 +0300

    Implemented CPUID and SysInfo feature functionality

--- interface.cpp
+++ interface.cpp
@@ -3,15 +3,17 @@
 //
 
 #include "interface.h"
+#include "sysinfo.h"
+
+#include <iomanip>
+#include <iostream>
 
 void mainMenu() {
-    std::cout << std::setw(40) << std::right << "Welcome to CPU Stress Test & Benchmark Tool\n";
-    std::cout << std::setw(40) << std::right << "-------------------------------------------\n";
     std::cout << std::setw(5) << std::left << "1." << std::right << "Stress Test\n";
     std::cout << std::setw(5) << std::left << "2." << std::right << "Benchmark\n";
     std::cout << std::setw(5) << std::left << "3." << std::right << "SysInfo\n";
     std::cout << std::setw(5) << std::left << "4." << std::right << "Exit\n";
-    std::cout << "Please enter your choice (1-4): ";
+    std::cout << "Please enter your choice (1-4): " << endl;
 }
 
 void stressTestMenu() {
@@ -23,5 +25,12 @@ void benchmarkMenu() {
 }
 
 void sysInfoMenu() {
+    CPUInfo cinfo;
 
+    cout << std::setw(5) << std::left << "CPU Vendor = " << std::right << cinfo.vendor() << endl;
+    cout << std::setw(5) << std::left << "CPU Brand = " << std::right << cinfo.model() << endl;
+    cout << std::setw(5) << std::left << "CPU Core Count = " << std::right << cinfo.cores() << endl;
+    cout << std::setw(5) << std::left << "CPU Logical Core Count = " << std::right << cinfo.logicalCpus() << endl;
+    cout << std::setw(5) << std::left << "CPU Hyper Threaded = " << std::right << cinfo.isHyperThreaded() << endl;
+    cout << endl;
 }
--- sysinfo.h
+++ sysinfo.h
@@ -7,7 +7,77 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
+#ifdef _WIN32
+#include <limits.h>
+#include <intrin.h>
+#else
+#include <stdint.h>
+#endif
 
+#define MAX_INTEL_TOP_LVL 4
+
+using namespace std;
+
+class CPUID {
+    uint32_t regs[4];
+
+public:
+    explicit CPUID(unsigned funcID, unsigned subFuncID) {
+#ifdef _WIN32
+        __cpuidex((int *)regs, (int)funcID, (int)subFuncID);
+
+#else
+        asm volatile("cpuid": "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
+                    : "a" (funcID), "c" (subFuncID));
+#endif
+    }
+
+    const uint32_t &EAX() const { return regs[0]; }
+    const uint32_t &EBX() const { return regs[1]; }
+    const uint32_t &ECX() const { return regs[2]; }
+    const uint32_t &EDX() const { return regs[3]; }
+
+};
+
+class CPUInfo {
+private:
+    static const uint32_t SSE_POS   = 0x02000000;
+    static const uint32_t SSE2_POS  = 0x04000000;
+    static const uint32_t SSE3_POS  = 0x00000001;
+    static const uint32_t SSE41_POS = 0x00080000;
+    static const uint32_t SSE42_POS = 0x00100000;
+    static const uint32_t AVX_POS   = 0x10000000;
+    static const uint32_t AVX2_POS  = 0x00000020;
+    static const uint32_t LVL_NUM   = 0x000000FF;
+    static const uint32_t LVL_TYPE  = 0x0000FF00;
+    static const uint32_t LVL_CORES = 0x0000FFFF;
+
+    // Attributes
+    string mVendorID;
+    string mModelName;
+    int    mNumSMT;
+    int    mNumCores;
+    int    mNumLogCpus;
+    float  mCPUMHz;
+    bool   mIsHTT;
+    bool   mIsSSE;
+    bool   mIsSSE2;
+    bool   mIsSSE3;
+    bool   mIsSSE41;
+    bool   mIsSSE42;
+    bool   mIsAVX;
+    bool   mIsAVX2;
+
+public:
+    CPUInfo();
+    string vendor() const { return mVendorID; }
+    string  model()             const { return mModelName;  }
+    int     cores()             const { return mNumCores;   }
+    float   cpuSpeedInMHz()     const { return mCPUMHz;     }
+    bool    isHyperThreaded()   const { return mIsHTT;      }
+    int     logicalCpus()       const { return mNumLogCpus; }
+};
 
 #endif //PROJECT_CLION_SYSINFO_H
